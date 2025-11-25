5d9ebf4eabd2c3991d49c6f5d7a691cba1e3c50e
MelaniaRyazanov <melania.ryazanov@gmail.com>
2025-11-25 09:28:58 -0800

    Update Shapes.java

--- Shapes.java
+++ Shapes.java
@@ -1,6 +1,10 @@
 public class Shapes {
     public static void main(String[] args) {
         // Task 1: Draw a regular shape
+        Turtle turtle = new Turtle();
+        
+         turtle.forward;
+   
 
 
         // Task 2: Draw a regular shape many times
