7e9291e4d185b22353eb31ffccd5f1261aabc951
MelaniaRyazanov <melania.ryazanov@gmail.com>
2025-11-25 09:40:19 -0800

    not a loop

--- Shapes.java
+++ Shapes.java
@@ -2,9 +2,15 @@ public class Shapes {
     public static void main(String[] args) {
         // Task 1: Draw a regular shape
         Turtle turtle = new Turtle();
-        
-         turtle.forward;
-   
+         turtle.forward(100);
+         turtle.right(100);
+         turtle.forward(100);
+         turtle.right(100);
+         turtle.forward(100);
+          turtle.right(100);
+         turtle.forward(100);
+       
+         
 
 
         // Task 2: Draw a regular shape many times
