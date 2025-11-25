f7c42428fdeb404fee55ea2fc6e42b405389136a
MelaniaRyazanov <melania.ryazanov@gmail.com>
2025-11-25 09:48:36 -0800

    Loop to make square

--- Shapes.java
+++ Shapes.java
@@ -2,13 +2,11 @@ public class Shapes {
     public static void main(String[] args) {
         // Task 1: Draw a regular shape
         Turtle turtle = new Turtle();
+        for(int i = 0; i <=4; i++){
          turtle.forward(100);
-         turtle.right(100);
-         turtle.forward(100);
-         turtle.right(100);
-         turtle.forward(100);
-          turtle.right(100);
-         turtle.forward(100);
+         turtle.right(90);
+         }
+       
        
          
 
