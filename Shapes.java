4b856543caa41a8253caf94883811af9ba7f3257
MelaniaRyazanov <melania.ryazanov@gmail.com>
2025-11-26 09:10:31 -0800

    turtle2 location

--- Shapes.java
+++ Shapes.java
@@ -2,16 +2,23 @@ public class Shapes {
     public static void main(String[] args) {
         // Task 1: Draw a regular shape
         Turtle turtle = new Turtle();
+        Turtle turtle2 = new Turtle(100,100);
+        
         for(int i = 0; i <=4; i++){
          turtle.forward(100);
          turtle.right(90);
          }
        
-       
-         
-
-
         // Task 2: Draw a regular shape many times
+        
+      for(int i = 0; i <=4; i++){  
+      turtle2.forward(100);
+      turtle2.right(90);
+      }
+      
+        
+
+
 
 
         // Task 3: How many times does Turtle turn!
