3d8218ae81f13d13a0abbca48b8b784962372fba
MelaniaRyazanov <melania.ryazanov@gmail.com>
2025-11-26 09:35:05 -0800

    finished

--- Shapes.java
+++ Shapes.java
@@ -21,7 +21,7 @@ public class Shapes {
         // Task 3: How many times does Turtle turn!
         int cumulativeSum = 0;
        
-        
+        // x = 1; x <=5; multiplied by i= 0; i<=4; is 5 x 5
         for(int x = 1; x <= 5; x++){
         for(int i = 0; i <=4; i++){  
          turtle3.forward(100);
