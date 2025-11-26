1880537b78d4d1ab76300d47ae72236578c042ab
MelaniaRyazanov <melania.ryazanov@gmail.com>
2025-11-26 09:30:28 -0800

    sum

--- Shapes.java
+++ Shapes.java
@@ -3,6 +3,7 @@ public class Shapes {
         // Task 1: Draw a regular shape
         Turtle turtle = new Turtle();
         Turtle turtle2 = new Turtle(100,100);
+        Turtle turtle3 = new Turtle(-100, -100);
         
         for(int i = 0; i <=4; i++){
          turtle.forward(100);
@@ -14,14 +15,22 @@ public class Shapes {
         for(int i = 0; i <=4; i++){  
          turtle2.forward(100);
          turtle2.right(90);
-         }}
+         }
+         }
       
-        
-
-
-
-
         // Task 3: How many times does Turtle turn!
+        int cumulativeSum = 0;
+       
+        
+        for(int x = 1; x <= 5; x++){
+        for(int i = 0; i <=4; i++){  
+         turtle3.forward(100);
+         turtle3.right(90);
+         cumulativeSum++;
+         }
+       
+         }
+         System.out.println("turtle3 has turned " + cumulativeSum + " times!");
 
 
         // Extension 1:
